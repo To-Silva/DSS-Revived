74d901e51e9ce2dc524f963b3761c222b9954971
To-Silva <a73827@alunos.uminho.pt>
2016-12-30 16:03:44 +0000

    Cenas e tal

--- src/Sistema/Gestao/Despesas/LN/Subsistema/Pagamentos/SPagamento.java
+++ src/Sistema/Gestao/Despesas/LN/Subsistema/Pagamentos/SPagamento.java
@@ -3,16 +3,26 @@ package Sistema.Gestao.Despesas.LN.Subsistema.Pagamentos;
 
 import java.time.LocalDateTime;
 
-public class SPagamento {
+public class SPagamento 
+{
 	private LocalDateTime Data;
 	private float valor;
 	private String nome;
 	private LocalDateTime DataPagamento;
 
-	public boolean atualizaPagamento() {
-            this.DataPagamento=LocalDateTime.now();
-            return true;
-	}
+    public SPagamento(LocalDateTime Data, float Valor) 
+    {
+        this.Data=Data;
+        valor=Valor;
+        nomes=null;
+        DataPagamento=null;
+    }
+
+    public boolean atualizaPagamento() 
+    {
+        this.DataPagamento=LocalDateTime.now();
+        return true;
+    }
         
         public boolean buscaPago(){
             return DataPagamento!=null;
